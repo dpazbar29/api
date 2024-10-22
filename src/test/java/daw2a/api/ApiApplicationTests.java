faece385a80130917a88de156afba9622cccbf75
dpazbar29 <dpaz@iesrafaelalberti.es>
2024-10-22 12:58:54 +0200

    Creacion Proyecto

---
+++ src/test/java/daw2a/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package daw2a.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
